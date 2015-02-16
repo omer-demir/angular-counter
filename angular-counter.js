8e9d3f8da025c51ed9c3b7ea2628e1ef731fbc4d
Ömer Bey <omer@nar.com.tr>
2015-02-16 22:25:58 +0200

    Initial Commit

---
+++ angular-counter.js
@@ -0,0 +1,32 @@
+(function(){
+	'use strict';
+	angular.module('angular-count',[]).directive('count-up',function($interval){
+		return{
+			scope:{
+				valueToCount:'=',
+				speed:'@'
+			},
+			restrict:'AEC',
+			replace:'true',
+			template:'<div class="well"><p class="text-center">{{valueToCount}}</p></div>'
+			link:function(scope){
+				scope.startPoint = 0;
+
+	            var timer = $interval(function () {
+	                if (scope.valueToCount!==0) {
+	                    scope.startPoint++;
+	                } else {
+	                    $interval.cancel(timer);
+	                }
+	            }, scope.speed);
+
+	            scope.$watch('startPoint', function (newValue, oldValue) {
+	                if (newValue === scope.valueToCount) {
+	                    $interval.cancel(timer);
+	                }
+	            });
+			}
+		};
+
+	});
+}());
